2ff62666a05a6ea2658c74fcab79380396405a69
Ritul-Patil <ritulpatil6@gmail.com>
2025-08-08 03:37:56 +0530

    Splash,Login,Register Activity Added --> Ritul Patil

---
+++ app/src/main/java/com/sphere/jobsphere/Activities/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.sphere.jobsphere.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.sphere.jobsphere.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(()->{
+            startActivity(new Intent(this,LoginActivity.class));
+            finish();
+        },2000);
+    }
+}
