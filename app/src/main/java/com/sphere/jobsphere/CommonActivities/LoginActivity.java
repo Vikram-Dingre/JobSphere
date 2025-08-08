337ac48ce45ed6411c009cc46a43c3e436a617d8
Vikram Dingre <vikramdingre91.cs@gmail.com>
2025-08-08 10:30:07 +0530

    Modified File Structure --> Vikram Dingre

--- app/src/main/java/com/sphere/jobsphere/CommonActivities/LoginActivity.java
+++ app/src/main/java/com/sphere/jobsphere/CommonActivities/LoginActivity.java
@@ -1,15 +1,11 @@
-package com.sphere.jobsphere.Activities;
+package com.sphere.jobsphere.CommonActivities;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.sphere.jobsphere.R;
 
--- app/src/main/java/com/sphere/jobsphere/CommonActivities/SplashActivity.java
+++ app/src/main/java/com/sphere/jobsphere/CommonActivities/SplashActivity.java
@@ -1,4 +1,4 @@
-package com.sphere.jobsphere.Activities;
+package com.sphere.jobsphere.CommonActivities;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -6,9 +6,6 @@ import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.sphere.jobsphere.R;
 
